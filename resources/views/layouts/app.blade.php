c3244a7d73597d1c5a3f2d88281559ee61295ac0
asanduk <asanduk@hotmail.com>
2024-09-11 12:33:48 +0200

    Boş klasörler için .gitkeep dosyası eklendi

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,8 +8,9 @@
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">
 
-        <!-- Styles -->
+        <!-- Vite Styles and Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
+
         @livewireStyles
     </head>
     <body class="font-sans antialiased">
